46f25164e047262e3f66bdf255d794c82dc9d579
Tousif <toship.kalalakond@qlsacademy.com>
2022-01-30 11:00:14 +0530

    [ADD] DB config

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,11 @@ WORKDIR /app
 EXPOSE 8000
 RUN python -m venv /django_env && \
     /django_env/bin/pip install --upgrade pip && \
+    # apk add --update --no-cache postgresql-client && \
+    # apk add --update --no-cache --vertual .tmp.deps \
+    #     build-base postgresql-dev musl-dev && \
     /django_env/bin/pip install -r /requirements.txt && \
+    # apk del .tmp-deps && \
     adduser --disabled-password --no-create-home django_user
 ENV PATH="/django_env/bin/:$PATH"
 USER django_user
--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c6#dqk1(^=6iw1xlm69%xhjrao+-%-_g%0)7=2tywt0ozbj@%0'
+SECRET_KEY = os.environ.get("SECREAT_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(",")
+    )
+)
 
 # Application definition
 
@@ -75,8 +80,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get("DB_HOST"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD")
     }
 }
 
